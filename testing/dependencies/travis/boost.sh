d86b0947277cb9da450f3305e2728c7d667c4dd9
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-02 15:25:45 -0400

    Give Homebrew another chance

--- testing/dependencies/travis/boost.sh
+++ testing/dependencies/travis/boost.sh
@@ -9,32 +9,47 @@ if [[ -f $HOME/Deps/boost/include/boost/version.hpp ]]; then
 else
     echo "-- Boost $Boost_VERSION NOT FOUND in cache"
     target_path=$HOME/Downloads/boost_"${Boost_VERSION//\./_}"
-    boost_url="https://sourceforge.net/projects/boost/files/boost/1.59.0/boost_${Boost_VERSION//\./_}.tar.gz"
+    boost_url="https://sourceforge.net/projects/boost/files/boost/$Boost_VERSION/boost_${Boost_VERSION//\./_}.tar.gz"
     mkdir -p "$target_path"
     curl -Ls "$boost_url" | tar -xz -C "$target_path" --strip-components=1
-    # Set the toolset
-    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-       toolset="gcc"
-    elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        toolset="darwin"
-    fi
     cd "$target_path"
-    # Configure
-    ./bootstrap.sh \
-        --with-toolset="$toolset" \
-        --with-libraries=filesystem,system,test,python \
-        --with-python="$PYTHON3" \
-        --prefix="$HOME/Deps/boost"
-    # Build and install
-    ./b2 -q install \
-         link=shared \
-         threading=multi \
-         variant=release \
-         toolset="$toolset" \
-         --with-filesystem \
-         --with-test \
-         --with-system \
-         --with-python
+    # Configure, build, and install
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+       # Configure
+       ./bootstrap.sh \
+           --with-toolset="gcc" \
+           --with-libraries=filesystem,system,test,python \
+           --with-python="$PYTHON3" \
+           --prefix="$HOME/Deps/boost" &> /dev/null
+       # Build and install
+       ./b2 -q install \
+            link=shared \
+            threading=multi \
+            variant=release \
+            toolset=gcc-7 \
+            --with-filesystem \
+            --with-test \
+            --with-system \
+            --with-python
+    elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        echo "Nothing to see here!"
+        ## Configure
+        #./bootstrap.sh \
+        #    --with-toolset="darwin" \
+        #    --with-libraries=filesystem,system,test,python \
+        #    --with-python="$PYTHON3" \
+        #    --prefix="$HOME/Deps/boost" &> /dev/null
+        ## Build and install
+        #./b2 -q install \
+        #     link=shared \
+        #     threading=multi \
+        #     variant=release \
+        #     toolset="darwin" \
+        #     --with-filesystem \
+        #     --with-test \
+        #     --with-system \
+        #     --with-python
+    fi
     cd "$TRAVIS_BUILD_DIR"
 fi
 echo "-- Done installing Boost $Boost_VERSION"
