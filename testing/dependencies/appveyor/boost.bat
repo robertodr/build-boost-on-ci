1c95674ce2507198bb29e1f462769eb8b30f26a5
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-03 12:03:51 -0400

    Silence and only build 64-bit

--- testing/dependencies/appveyor/boost.bat
+++ testing/dependencies/appveyor/boost.bat
@@ -17,10 +17,9 @@ if "%nonVSGenerator%"=="true" (
   
   rem Configure
   cd boost_1_67_0
-  bootstrap.bat
-  rem --with-toolset=msvc --with-python="C:\Python37-x64\python.exe" --with-libraries=filesystem,system,test,python --prefix="C:\Deps\boost_1.66.0"
+  bootstrap.bat > NUL
   rem Build and install
-  b2 -q install link=static,shared threading=multi variant=release toolset=msvc --with-filesystem --with-test --with-system --with-python --prefix="C:\Deps\boost_1_67_0"
+  b2 -q -d0 install link=static,shared threading=multi variant=release toolset=msvc address-model=64 --with-filesystem --with-test --with-system --with-python --prefix="C:\Deps\boost_1_67_0"
   rem Clean up
   cd ..
   rmdir boost_1_67_0 boost_1_67_0.zip
