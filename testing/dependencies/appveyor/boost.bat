3d07c921b2eb920904309a8559d529810d2b8c82
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-03 18:27:52 -0400

    Final touches

--- testing/dependencies/appveyor/boost.bat
+++ testing/dependencies/appveyor/boost.bat
@@ -1,28 +1,29 @@
-rem Finally found a use for De Morgan's laws of boolean algebra!
-rem We can't use logical OR in IF statements, so we check for the negation of
-rem the AND (implicit when chaining IF-s) of the negation of each separate statement
-set nonVSGenerator=true
-if not "%CMAKE_GENERATOR%"=="Ninja" if not "%CMAKE_GENERATOR%"=="MSYS Makefiles" set nonVSGenerator=false
+rem Check whether Boost libraries compiled with MSVC and Python 3.7 are in cache
+rem If not, download and build them. We do not build all of Boost, just the
+rem bits we need: filesystem, test, system, and python.
 
-if "%nonVSGenerator%"=="true" (
+if "%CMAKE_GENERATOR%"=="Visual Studio 15 2017 Win64" (
   echo "Using non-VS generator %CMAKE_GENERATOR%"
   echo "Nothing to do here!"
 ) else (
   echo "Using VS generator %CMAKE_GENERATOR%"
-  echo "Configuring, building, and installing Boost"
-
-  rem Download
-  bash -c "curl -LOs https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.zip"
-  bash -c "7z x boost_1_67_0.zip"
-  
-  rem Configure
-  cd boost_1_67_0
-  bootstrap.bat > NUL
-
-  rem Build and install
-  b2 -q -d0 install link=static,shared threading=multi variant=release toolset=msvc address-model=64 --with-filesystem --with-test --with-system --with-python --prefix="C:\Deps\boost_1_67_0"
-
-  rem Clean up
-  cd %APPVEYOR_BUILD_FOLDER%
-  del /s /q boost_1_67_0 boost_1_67_0.zip
+  echo "-- Installing Boost"
+  if exist "C:\Deps\boost_1_67_0\include\boost\version.hpp" (
+    echo "-- Boost FOUND in cache"
+  ) else (
+    echo "-- Boost NOT FOUND in cache"
+    rem Download
+    bash -c "curl -LOs https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.zip"
+    bash -c "7z x boost_1_67_0.zip"
+    rem Configure
+    cd boost_1_67_0
+    bootstrap.bat > NUL
+    rem Build and install
+    b2 -q install link=static,shared threading=multi variant=release toolset=msvc address-model=64 --with-filesystem --with-test --with-system --with-python --prefix="C:\Deps\boost_1_67_0" > NUL
+    rem Clean up
+    cd %APPVEYOR_BUILD_FOLDER%
+    del /s /q boost_1_67_0 boost_1_67_0.zip > NUL
+  )
+  echo "-- Done installing Boost"
+  set BOOST_ROOT=C:\Deps\boost_1_67_0
 )
