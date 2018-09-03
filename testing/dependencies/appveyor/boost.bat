e64fce6ec106aead804f81cd2153290ac5ccb59a
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-03 17:56:04 -0400

    What the funk

--- testing/dependencies/appveyor/boost.bat
+++ testing/dependencies/appveyor/boost.bat
@@ -23,6 +23,8 @@ if "%nonVSGenerator%"=="true" (
   b2 -q -d0 install link=static,shared threading=multi variant=release toolset=msvc address-model=64 --with-filesystem --with-test --with-system --with-python --prefix="C:\Deps\boost_1_67_0"
 
   rem Clean up
+  dir .
   cd %APPVEYOR_BUILD_FOLDER%
+  dir .
   rmdir boost_1_67_0 boost_1_67_0.zip
 )
