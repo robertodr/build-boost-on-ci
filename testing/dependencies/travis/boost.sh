72094fa88217fd5e4580e53a8a8573c46c809df6
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-02 23:15:50 -0400

    Retry with brew

--- testing/dependencies/travis/boost.sh
+++ testing/dependencies/travis/boost.sh
@@ -32,26 +32,28 @@ else
             --with-system \
             --with-python &> /dev/null
     elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        # With help from: https://thb.lt/blog/notes/2014/boost-python-3-osx.html
-        # Configure
-        ./bootstrap.sh \
-            --with-toolset=clang \
-            --with-libraries=filesystem,system,test,python \
-            --with-python-version=3.7 \
-            --with-python-root=$(python3-config --prefix) \
-            --with-python=$(python3-config --prefix)/bin/python3.7 \
-            --prefix="$HOME/Deps/boost" &> /dev/null
-        # Build and install
-        ./b2 -q install \
-             include="$(python3-config --prefix)/include/python3.7m" \
-             link=shared \
-             threading=multi \
-             variant=release \
-             toolset=clang \
-             --with-filesystem \
-             --with-test \
-             --with-system \
-             --with-python
+        brew install boost
+        brew install boost-python3
+        ## With help from: https://thb.lt/blog/notes/2014/boost-python-3-osx.html
+        ## Configure
+        #./bootstrap.sh \
+        #    --with-toolset=clang \
+        #    --with-libraries=filesystem,system,test,python \
+        #    --with-python-version=3.7 \
+        #    --with-python-root=$(python3-config --prefix) \
+        #    --with-python=$(python3-config --prefix)/bin/python3.7 \
+        #    --prefix="$HOME/Deps/boost" &> /dev/null
+        ## Build and install
+        #./b2 -q install \
+        #     include="$(python3-config --prefix)/include/python3.7m" \
+        #     link=shared \
+        #     threading=multi \
+        #     variant=release \
+        #     toolset=clang \
+        #     --with-filesystem \
+        #     --with-test \
+        #     --with-system \
+        #     --with-python
     fi
     cd "$TRAVIS_BUILD_DIR"
 fi
